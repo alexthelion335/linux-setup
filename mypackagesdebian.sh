ef421519b7ad610027f79864c41d9349dc34dffe
alexthelion335 <alexthelion335@gmail.com>
2020-06-17 01:34:54 -0400

    Update mypackagesdebian.sh

--- mypackagesdebian.sh
+++ mypackagesdebian.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt update && sudo apt upgrade
-sudo apt install firefox konsole plasma dolphin i3gaps i3lock i3status steam inkscape gimp networkmanager dhcpcd screenfetch lolcat lynx mutt cmatrix
+sudo apt install firefox konsole plasma dolphin i3gaps i3lock i3status steam inkscape gimp networkmanager dhcpcd screenfetch lolcat lynx mutt cmatrix compton feh
