9bb0d9f1655d62aa9fa3dcaa597b75bf20cdcfce
alexthelion335 <alexthelion335@gmail.com>
2020-07-18 20:24:22 +0000

    Update archpackages.sh

--- archpackages.sh
+++ archpackages.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 sudo pacman -Syu
-sudo pacman -S nano vim sudo networkmanager dhcpcd grub xorg-server xf86-video-vesa xorg-xinit neofetch i3gaps i3lock i3status feh brave konsole xterm mpv thunar dolphin
+sudo pacman -S nano vim sudo networkmanager dhcpcd grub xorg-server xf86-video-vesa xorg-xinit neofetch i3gaps i3lock i3status feh brave konsole xterm mpv thunar dolphin picom inkscape gimp cmatrix lynx lolcat cowsay
