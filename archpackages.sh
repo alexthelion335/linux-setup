f446ffa97537a8a6d958d50d8a033e4f4f91e970
alexthelion335 <alexthelion335@gmail.com>
2020-07-18 20:21:16 +0000

    Create archpackages.sh

---
+++ archpackages.sh
@@ -0,0 +1,2 @@
+sudo pacman -Syu
+sudo pacman -S nano vim sudo networkmanager dhcpcd grub xorg-server xf86-video-vesa xorg-xinit neofetch i3gaps i3lock i3status feh brave konsole xterm mpv thunar dolphin
